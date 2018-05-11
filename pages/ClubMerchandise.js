be61b8740231b96877e526be731af095987bbc7c
Peter Vargovcik <peter.vargovcik@gmail.com>
2018-05-11 14:38:38 +0100

    edited the Merchandise Page

--- pages/ClubMerchandise.js
+++ pages/ClubMerchandise.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { View, Text, Button, Icon, Card,StyleSheet,Image,TouchableOpacity, Linking } from 'react-native';
+import { View, Text, Button, Icon, Card,StyleSheet,Image,TouchableOpacity, Linking, Dimensions } from 'react-native';
 import AppStyle from '../styles/AppStyle.js'
 import BackgroundTheme from '../views/BackgroundTheme.js'
 class ClubMerchandiseScreen extends React.Component{
   constructor(props) {
     super(props);
 
-    this.state = {      
+    let windowWidth = Dimensions.get('window').width;
+
+    this.state = {         
+      screenWidth : windowWidth,   
       shopURL : "https://www.oneills.com/shop-by-team/gaa/ireland/garrycastle-gaa.html",
     };
 
@@ -28,19 +31,40 @@ class ClubMerchandiseScreen extends React.Component{
 
   render() {
     return (
-      <BackgroundTheme>
-        <View style={{ 
-          flex: 1}}>
-        <Text style={AppStyle.fixturesDivisionHeading}> Our Club is happy to announce the availability of club gear which can now be purchased directly from the O'Neills Sports on-line shop. </Text> 
-         {/* Shop Button */}
-         <TouchableOpacity 
-          style={AppStyle.buttonContainer} 
-          activeOpacity={0.5}
-          onPress={() =>this.gotoONeals()}> 
-          <Image source={require('../images/shopping-cart.png')} style={AppStyle.buttonImage} /> 
-          <Text style={AppStyle.buttonText}> Club Gear </Text> 
-       </TouchableOpacity>
+      <BackgroundTheme>     
+        <View style={{ flex: 1}}>
+
+          <View style={{backgroundColor: 'rgba(0,0,0,0.4)', borderTopWidth: 3, borderBottomWidth: 3, borderColor: 'white',marginTop: 20, marginBottom: 20}}>             
+            <Text style={{fontSize: 23, color : 'white', textAlign: 'center', padding:5 }}>Our Club is happy to announce the availability of club gear which can now be purchased directly from the O'Neills Sports on-line shop.</Text>
+          </View>
+
+          {/* Shop Button */}
+          <TouchableOpacity 
+              style={{
+                alignItems: 'center',
+                backgroundColor: 'rgba(0,0,0,0.4)',
+                borderWidth: 3,
+                borderColor: '#fff',
+                margin: 5,    
+              }} 
+              activeOpacity={0.5}
+              onPress={() =>this.gotoONeals()}> 
+              <Image source={require('../images/shopping-cart-large.png')} style={{
+                paddingBottom : 10,
+                height: 150,
+                width: 150,
+                resizeMode : 'stretch',    
+              }} /> 
+              <Text style={{ 
+                fontSize: 40,
+                color: "#fff",
+                marginBottom : 4, 
+                fontFamily: 'arial',  
+              }}> Club Gear </Text> 
+          </TouchableOpacity>
+
         </View>
+        <Image  style={{width: this.state.screenWidth, height: this.state.screenWidth *.4}} source={require("../images/players.png")}/>
       </BackgroundTheme>
     );
   }
