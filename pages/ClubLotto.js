1e7ab7eed7c0f01ff90c7108f115b502abd241db
Peter Vargovcik <peter.vargovcik@gmail.com>
2018-05-11 12:41:22 +0100

    altered Lotto page

--- pages/ClubLotto.js
+++ pages/ClubLotto.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { View, Text, Button, Icon, Card,StyleSheet,Image, Props  } from 'react-native';
+import { View, Text, Button, Icon, Card,StyleSheet,Image, Props,ImageBackground,Dimensions  } from 'react-native';
 import BackgroundTheme from '../views/BackgroundTheme';
 class ClubLottoScreen extends React.Component {
   constructor(props){
     super(props);
 
+    let windowWidth = Dimensions.get('window').width;
+
     this.state = {
-      results: [1,2,33,4]
+      screenWidth : windowWidth,
+      day : "Monday May 8th 2018",
+      results: [99,2,33,4]
     };
   }
 
@@ -23,19 +27,30 @@ class ClubLottoScreen extends React.Component {
     render(){
       return(
         <BackgroundTheme>
-          <View style={styles.container}>
-            <View style={styles.lottonumbers}>
-            <Text style={{fontSize: 23, color : 'white'}}>The Lotto numbers for Monday May 8th 2018 were</Text>
+        
+          <View style={{backgroundColor: 'rgba(0,0,0,0.4)', borderTopWidth: 3, borderBottomWidth: 3, borderColor: 'white',marginTop: 20, marginBottom: 20}}>             
+            <Text style={{fontSize: 23, color : 'white', textAlign: 'center'}}>The Lotto numbers for {this.state.day} were</Text>
+          </View>
+
+          <View style={{backgroundColor: 'rgba(0,0,0,0.4)', borderTopWidth: 3, borderBottomWidth: 3, borderColor: 'white', alignItems: 'center'}}> 
+            <View style={{ flexDirection: 'row', paddingTop : 20, paddingBottom : 20,  width: this.state.screenWidth }}>
+              <ImageBackground style={styles.lottoBall} source={require('../images/loto-ball-inverted.png')}>            
+                <Text style={styles.lottoBallText}>{this.state.results[0]}</Text>
+              </ImageBackground>
+              <ImageBackground style={styles.lottoBall} source={require('../images/loto-ball-inverted.png')}>            
+                <Text style={styles.lottoBallText}>{this.state.results[1]}</Text>
+              </ImageBackground>
+              <ImageBackground style={styles.lottoBall} source={require('../images/loto-ball-inverted.png')}>            
+                <Text style={styles.lottoBallText}>{this.state.results[2]}</Text>
+              </ImageBackground>
+              <ImageBackground style={styles.lottoBall} source={require('../images/loto-ball-inverted.png')}>            
+                <Text style={styles.lottoBallText}>{this.state.results[3]}</Text>
+              </ImageBackground>
             </View>
-          <View style={styles.results}>
-          
-          <Text style={{fontSize: 90, color:'white'}}>{this.state.results[0]},</Text>
-          <Text style={{fontSize: 90, color:'white'}}>{this.state.results[1]},</Text>
-            <Text style={{fontSize: 90, color:'white'}}>{this.state.results[2]},</Text>
-              <Text style={{fontSize: 90, color:'white'}}>{this.state.results[3]},</Text>
-           
-          </View>
-          </View>
+
+            <Image  style={{width: 200, height: 190 ,marginBottom: 20}} source={require("../images/loto-jar-inverted.png")}/>
+          </View>         
+            
        </BackgroundTheme>
      );
     
@@ -43,21 +58,34 @@ class ClubLottoScreen extends React.Component {
   }
 
 
-  export default ClubLottoScreen;
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      
-    },
- lottonumbers: {
-    alignItems: 'center',
+export default ClubLottoScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
     
- },
- results: {
+  },
+  lottonumbers: {
+      alignItems: 'center',
+      
+  },
+  results: {
      alignItems: 'center',
      borderWidth: 3,
      flexDirection: 'row',
      backgroundColor: 'rgba(0,0,0,0.4)',
      paddingLeft:45,
- }
+  },
+  lottoBall : {
+    flex: 2,
+    alignItems: 'center',
+    width : 50,
+    height : 90
+  },
+  lottoBallText : {
+    paddingTop :7, 
+    fontSize: 40, 
+    color:'white',
+    fontWeight: 'bold'
+  }
 });
